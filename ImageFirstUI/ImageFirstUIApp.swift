78b86e7c9dfcb52babfb8da83b99ca78c0986bae
Hervé LEMAI <hlemai@vekia.fr>
2020-12-31 17:31:52 +0100

    Initial Commit

---
+++ ImageFirstUI/ImageFirstUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFirstUIApp.swift
+//  ImageFirstUI
+//
+//  Created by Hervé LEMAI on 31/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ImageFirstUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
